7a80c6ac858035350bb68ebb646651a8d6f9a565
jackrodgers3 <jackmadscience333@gmail.com>
2023-06-24 17:57:13 -0400

    add file

--- office_transformer.py
+++ office_transformer.py
@@ -3,18 +3,24 @@ import torch.nn as nn
 from torch.nn import functional as F
 torch.manual_seed(23)
 #hyperparameters
-batch_size = 32
-block_size = 8
-learning_rate = 1e-3
-max_iters = 6000
-eval_interval = 300
+batch_size = 64 # how many independent sequences we process in parallel
+block_size = 256 #maximum context length for predictions
+learning_rate = 4e-4
+max_iters = 8000
+eval_interval = 500
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
-
+n_embd = 384
+n_layer = 6
+n_head = 6
+dropout = 0.2
+printed_tokens = 2000
+print("Device:", device)
 
 with open("Data/office_transcripts.txt", 'r', encoding='utf-8') as f:
     text = f.read()
+
+
 
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
@@ -25,21 +31,10 @@ print("Number of characters:", len(chars))
 def preprocess_text(text):
     ind1 = 0
     ind2 = 0
-    counts = 0
-    newtext = ""
     while ind1 != -1:
-        if newtext == "":
-            ind1 = text.find("[")
-            ind2 = text.find("]")
-        else:
-            ind1 = newtext.find("[")
-            ind2 = newtext.find("]")
-        if ind1 != -1:
-            newtext = text[ind2+2:]
-            text = text[:ind1-2]+text[ind2+2:]
-            counts = counts + 1
-            print(counts)
-
+        ind1 = text.find("[")
+        ind2 = text.find("]")
+        text = text[:ind1-1]+text[ind2+1:]
     return text
 
 
@@ -85,7 +80,7 @@ class Head(nn.Module):
         self.key = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
-
+        self.dropout = nn.Dropout(dropout)
     def forward(self, x):
         B, T, C = x.shape
         k = self.key(x)
@@ -94,13 +89,51 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2, -1) * C**-0.5
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
         wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
         v = self.value(x)
         out = wei @ v
         return out
 
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
 
 
+class FeedForward(nn.Module):
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
+    def forward(self, x):
+        return self.net(x)
+
+class Block(nn.Module):
+    #transformer block: communication then computation
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
 
 class BiGramLanguageModel(nn.Module):
     def __init__(self):
@@ -108,7 +141,8 @@ class BiGramLanguageModel(nn.Module):
         #each token leads off logits for next token
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd) #(B, T, C)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) #final norm layer
         self.lm_head = nn.Linear(n_embd, vocab_size) #(B, T, vocab_size)
     def forward(self, idx, targets = None):
         B, T = idx.shape
@@ -116,7 +150,7 @@ class BiGramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) #(T, C)
         x = tok_emb + pos_emb # x holds token identities and the positions at which the tokens occur
-        x = self.sa_head(x)
+        x = self.blocks(x)
         logits = self.lm_head(x)
         if targets is None:
             loss = None
@@ -146,7 +180,7 @@ xb, yb = get_batch('train')
 
 logits, loss = model(xb, yb)
 idx = torch.zeros((1,1), dtype=torch.long)
-print(decode(model.generate(idx, max_new_tokens=500)[0].tolist()))
+print(decode(model.generate(idx, max_new_tokens=printed_tokens)[0].tolist()))
 #optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
@@ -167,4 +201,4 @@ for iter in range(max_iters):
 
 
 context = torch.zeros((1,1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+print(decode(m.generate(context, max_new_tokens=printed_tokens)[0].tolist()))
