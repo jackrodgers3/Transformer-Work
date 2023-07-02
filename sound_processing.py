ce9b09219fcd9f77203af1b9b75bf306934d44c6
Jack <jackprodgers03@gmail.com>
2023-07-02 16:58:36 -0400

    add transcripts.py

--- sound_processing.py
+++ sound_processing.py
@@ -1,7 +1,8 @@
 import string
-
+import numpy as np
 import mido
 import os
+import matplotlib.pyplot as plt
 
 file_list = os.listdir(r'C:\Users\jackm\PycharmProjects\Transformer-Work\Data\Beethoven_MIDI')
 
@@ -33,11 +34,59 @@ for j, track in enumerate(mid_list[0].tracks):
     print('Track {}: {}'.format(j, track.name))
     if j == 1 or j == 2:
         for msg in track:
-            print(msg2dict(msg))
+            print(msg)
 
 '''
 for m in mid_list[0].tracks[1]:
     print(m)
 '''
+def switch_note(last_state, note, velocity, on_=True):
+    # piano has 88 notes, corresponding to note id 21 to 108, any note out of this range will be ignored
+    result = [0] * 88 if last_state is None else last_state.copy()
+    if 21 <= note <= 108:
+        result[note-21] = velocity if on_ else 0
+    return result
 
+def get_new_state(new_msg, last_state):
+    new_msg, on_ = msg2dict(str(new_msg))
+    new_state = switch_note(last_state, note=new_msg['note'], velocity=new_msg['velocity'], on_=on_) if on_ is not None else last_state
+    return [new_state, new_msg['time']]
+def track2seq(track):
+    # piano has 88 notes, corresponding to note id 21 to 108, any note out of the id range will be ignored
+    result = []
+    last_state, last_time = get_new_state(str(track[0]), [0]*88)
+    for i in range(1, len(track)):
+        new_state, new_time = get_new_state(track[i], last_state)
+        if new_time > 0:
+            result += [last_state]*new_time
+        last_state, last_time = new_state, new_time
+    return result
+
+def mid2arry(mid, min_msg_pct=0.1):
+    tracks_len = [len(tr) for tr in mid.tracks]
+    min_n_msg = max(tracks_len) * min_msg_pct
+    # convert each track to nested list
+    all_arys = []
+    for i in range(len(mid.tracks)):
+        if len(mid.tracks[i]) > min_n_msg:
+            ary_i = track2seq(mid.tracks[i])
+            all_arys.append(ary_i)
+    # make all nested list the same length
+    max_len = max([len(ary) for ary in all_arys])
+    for i in range(len(all_arys)):
+        if len(all_arys[i]) < max_len:
+            all_arys[i] += [[0] * 88] * (max_len - len(all_arys[i]))
+    all_arys = np.array(all_arys)
+    all_arys = all_arys.max(axis=0)
+    # trim: remove consecutive 0s in the beginning and at the end
+    sums = all_arys.sum(axis=1)
+    ends = np.where(sums > 0)[0]
+    return all_arys[min(ends): max(ends)]
+
+result_array = mid2arry(mid_list[0])
+print(result_array)
+
+plt.plot(range(result_array.shape[0]), np.multiply(np.where(result_array>0, 1, 0), range(1, 89)), marker='.', markersize=1, linestyle='')
+plt.title("beethoven_opus_10_1.mid")
+plt.show()
 
